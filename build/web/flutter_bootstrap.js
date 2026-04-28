e54a01b6582967e26a43a99e1575a7d84134cf82
masa.gongon2011 <masa.gongon2011@gmail.com>
2026-04-28 04:51:43 +0000

    build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4279334078"
+    serviceWorkerVersion: "535077359"
   }
 });
